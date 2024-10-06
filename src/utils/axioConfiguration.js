ddc4b9fffc3657fd3496176ab89e8f87dbdfe1e3
Abhisha <abhisha02@gmail.com>
2024-10-06 16:26:04 +0530

    url changed to backend

--- src/utils/axioConfiguration.js
+++ src/utils/axioConfiguration.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8000/',
+  baseURL: 'https://stockimagegallerybackend.online/',
+  
   withCredentials: true,
 });
 
